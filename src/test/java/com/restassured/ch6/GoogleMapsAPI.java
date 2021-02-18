7cfdff02960dabc010b6d00f843702760b12e5f1
Tim Brown <brownti28@yahoo.com>
2021-02-17 23:09:29 -0800

    ig

--- src/test/java/com/restassured/ch6/GoogleMapsAPI.java
+++ src/test/java/com/restassured/ch6/GoogleMapsAPI.java
@@ -3,9 +3,39 @@ package com.restassured.ch6;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static io.restassured.RestAssured.given;
 
 public class GoogleMapsAPI {
+
+    private static String KEY;
+
+    /**
+     * Static initializer instead of using a method.
+     */
+    static {
+        try (InputStream input = new FileInputStream(
+                "src/main/resources/invoice.properties")) {
+
+            Properties prop = new Properties();
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            KEY = prop.getProperty("google.api");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
 
 
     @BeforeClass
@@ -25,7 +55,7 @@ public class GoogleMapsAPI {
                 .param("units", "imperial")
                 .param("origins", "Seattle,WA")
                 .param("destination", "Portland,OR")
-                .param("key", "AIzaSyCkuTZAh86A5xLrFBamtnHR95K_t568ebc")
+                .param("key", KEY)
         .when()
                 .get("/distancematrix/json")
         .then()
